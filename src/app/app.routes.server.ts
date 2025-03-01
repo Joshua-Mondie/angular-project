dca01f117ebb5077b5889761a62760c9e5ba9ea1
Joseph Udonsak <joseph.udonsak@ibn.com.ng>
2025-03-01 22:06:55 +0000

    updates

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'users/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Fetch user data from API at build time
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const users = await response.json();
+
+      // Return a list of user IDs for prerendering
+      return users.map((user: { id: number }) => ({ id: user.id.toString() }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender, // Keep this for static pages
+  },
 ];
